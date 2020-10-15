fefc068db7f02e4d5ca8878b9ca72b87d60ede58
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2020-10-15 16:13:06 +0200

    Create script

---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+set -e
+
+# Setup these env variables.
+# - LABELS
+# - PR_NUMBER
+# - PR_TITLE
+setup_from_push_event() {
+  pull_request="$(list_pulls | jq ".[] | select(.merge_commit_sha==\"${GITHUB_SHA}\")")"
+  LABELS=$(echo "${pull_request}" | jq '.labels | .[].name')
+  PR_NUMBER=$(echo "${pull_request}" | jq -r .number)
+  PR_TITLE=$(echo "${pull_request}" | jq -r .title)
+}
+
+list_pulls() {
+  pulls_endpoint="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls?state=closed&sort=updated&direction=desc"
+  if [ -n "${INPUT_GITHUB_TOKEN}" ]; then
+    curl -s -H "Authorization: token ${INPUT_GITHUB_TOKEN}" "${pulls_endpoint}"
+  else
+    echo "INPUT_GITHUB_TOKEN is not available. Subscequent GitHub API call may fail due to API limit." >&2
+    curl -s "${pulls_endpoint}"
+  fi
+}
+
+setup_from_push_event
+
+BUMP_LEVEL="${INPUT_DEFAULT_BUMP_LEVEL}"
+if echo "${LABELS}" | grep "bump:major" ; then
+  BUMP_LEVEL="major"
+elif echo "${LABELS}" | grep "bump:minor" ; then
+  BUMP_LEVEL="minor"
+elif echo "${LABELS}" | grep "bump:patch" ; then
+  BUMP_LEVEL="patch"
+fi
+
+if [ -z "${BUMP_LEVEL}" ]; then
+  echo "PR with labels for bump not found. Do nothing."
+  echo "::set-output name=skipped::true"
+  exit
+fi
+echo "::set-output name=level::#{BUMP_LEVEL}"
+echo "Bump ${BUMP_LEVEL} version"
+
+if [ "${INPUT_DRY_RUN}" = "true" ]; then
+  echo
+  exit
+fi
+
+gem bump --commit --push --version ${BUMP_LEVEL}
